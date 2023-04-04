42f902810beaed60f64e8e32196e6bd8d71f3367
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-04-04 13:32:05 -0300

    Adição de inputs para alteração dos links

--- main.js
+++ main.js
@@ -1,24 +1,41 @@
-const linksSocialMedia = {
-  github: 'VictorGM01',
-  youtube: '/channel/UCHsp6_5KZ8iLuj6O1RHMXPg',
-  instagram: 'victor_gabriiielll/',
-  facebook: 'victorgabriel2017',
-  linkedin: '/in/victor-gabriel-marques-4a327a208/'
-}
+// Armazena os valores digitados no prompt
+let github = prompt('Digite aqui o seu usuário do GitHub');
+let youtube = prompt('Digite aqui o link do seu YouTube');
+let instagram = prompt('Digite aqui o seu usuário do Instagram');
+let facebook = prompt('Digite aqui o link do seu facebook');
+let linkedin = prompt('Digite aqui o link do seu LinkedIn');
 
-//function to change the social media links according to the user (função para mudar os links das redes sociais de acordo com o usuário)
+// Objeto que contém os trechos dos links das redes sociais
+const linksSocialMedia = {
+  github: github,
+  youtube: youtube,
+  instagram: instagram,
+  facebook: facebook,
+  linkedin: linkedin
+};
+
+// Armazena o elemento ul
+const socialLinks = document.querySelector('#socialLinks');
+
+//função para mudar os links das redes sociais de acordo com o usuário
 function changeSocialMediaLinks() {
+  // Itera pelos itens de ul
   for (let li of socialLinks.children) {
-    const social = li.getAttribute('class')
-    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    // Armazena a classe das tags li
+    const social = li.getAttribute('class');
+    // Pega o primeiro filho de li e muda o href
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
   }
 }
 
 changeSocialMediaLinks();
 
+// Função que entra em contato com a API do GitHub
 function getGitHubProfileInfos() {
+  // Armazena a URL
   const url = `https://api.github.com/users/${linksSocialMedia.github}`;
 
+  // Realiza o contato com a API
   fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -27,7 +44,7 @@ function getGitHubProfileInfos() {
       userLink.href = data.html_url;
       userPhoto.src = data.avatar_url;
       userLogin.textContent = data.login;
-    })
+    });
 }
 
 getGitHubProfileInfos();
